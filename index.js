e45f0e128509f02779c557630650f990f68123e3
Tobi Okanlawon <tobiinlondon34@gmail.com>
2023-06-04 17:58:09 +0100

    Add express to project to solve CORS problem

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const express = require("express");
+
+const expressInstance = express();
+const port = 7757;
+
+const options = {
+  root: path.join(__dirname, "timer", "dist"),
+};
+
+expressInstance.get("/", (req, res) => {
+  res.sendFile("index.html", options);
+});
+
+expressInstance.use(
+  "/assets",
+  express.static(path.join(options.root, "assets"))
+);
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -6,18 +24,16 @@ const createWindow = () => {
     height: 600,
   });
 
-  win.loadURL("http://localhost:5173/");
-  // win.loadFile("index.html");
+  win.loadURL(`http://localhost:${port}`);
 };
 
-app.whenReady().then(() => {
-  createWindow();
-});
-
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+const readyCallback = () => {
+  app.whenReady().then(() => {
+    createWindow();
+    app.on("activate", () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
   });
-});
+};
+
+expressInstance.listen(port, readyCallback);
